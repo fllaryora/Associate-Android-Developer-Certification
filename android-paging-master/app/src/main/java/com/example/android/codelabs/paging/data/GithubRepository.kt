14a63b91d983ae3a38481a82da0c69e84d7a21ef
fllaryora <fllaryora@gmail.com>
2019-04-08 00:38:18 -0300

    boundaryCallback

--- android-paging-master/app/src/main/java/com/example/android/codelabs/paging/data/GithubRepository.kt
+++ android-paging-master/app/src/main/java/com/example/android/codelabs/paging/data/GithubRepository.kt
@@ -16,11 +16,9 @@
 
 package com.example.android.codelabs.paging.data
 
-import android.arch.lifecycle.MutableLiveData
 import android.arch.paging.LivePagedListBuilder
 import android.util.Log
 import com.example.android.codelabs.paging.api.GithubService
-import com.example.android.codelabs.paging.api.searchRepos
 import com.example.android.codelabs.paging.db.GithubLocalCache
 import com.example.android.codelabs.paging.model.RepoSearchResult
 
@@ -32,55 +30,36 @@ class GithubRepository(
         private val cache: GithubLocalCache
 ) {
 
-    // keep the last requested page. When the request is successful, increment the page number.
-    private var lastRequestedPage = 1
-
-    // LiveData of network errors.
-    private val networkErrors = MutableLiveData<String>()
-
-    // avoid triggering multiple requests in the same time
-    private var isRequestInProgress = false
-
     /**
      * Search repositories whose names match the query.
+     *
+     * In the search() method, construct the RepoBoundaryCallback
+     * using the query, service, and cache.
+     * Create a value in the search() method that maintains a reference to the network errors
+     * that RepoBoundaryCallback discovers.
+     * Set the boundary callback to LivePagedListBuilder.
      */
     fun search(query: String): RepoSearchResult {
         Log.d("GithubRepository", "New query: $query")
+
         // Get data source factory from the local cache
         val dataSourceFactory = cache.reposByName(query)
 
-        // Get the paged list
-        val data = LivePagedListBuilder(dataSourceFactory, DATABASE_PAGE_SIZE).build()
+        // Construct the boundary callback
+        val boundaryCallback = RepoBoundaryCallback(query, service, cache)
+        val networkErrors = boundaryCallback.networkErrors
 
-        /**
-         *The LivePagedListBuilder is constructed using the dataSourceFactory
-         * and the database page size that you each defined earlier.
-         */
+        // Get the paged list
+        val data = LivePagedListBuilder(dataSourceFactory, DATABASE_PAGE_SIZE)
+                .setBoundaryCallback(boundaryCallback)
+                .build()
+
         // Get the network errors exposed by the boundary callback
         return RepoSearchResult(data, networkErrors)
     }
 
-    fun requestMore(query: String) {
-        requestAndSaveData(query)
-    }
-
-    private fun requestAndSaveData(query: String) {
-        if (isRequestInProgress) return
-
-        isRequestInProgress = true
-        searchRepos(service, query, lastRequestedPage, NETWORK_PAGE_SIZE, { repos ->
-            cache.insert(repos, {
-                lastRequestedPage++
-                isRequestInProgress = false
-            })
-        }, { error ->
-            networkErrors.postValue(error)
-            isRequestInProgress = false
-        })
-    }
 
     companion object {
-        private const val NETWORK_PAGE_SIZE = 50
         private const val DATABASE_PAGE_SIZE = 20
     }
 }
